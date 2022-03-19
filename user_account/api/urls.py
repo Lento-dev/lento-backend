21b0358a6c98bfa682df8293ec917482c755f0e1
sina <sinaeskandari007@gmail.com>
2022-03-19 15:17:51 +0330

    Add edit profile

--- user_account/api/urls.py
+++ user_account/api/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from user_account.api import views
-from user_account.api.views import(
-	DeleteAccount,
-	registration_view,
+from user_account.api.views import (
+    DeleteAccount,
+    registration_view,
 
 )
 
@@ -15,15 +15,15 @@ from rest_framework.authtoken.views import obtain_auth_token
 app_name = 'account'
 
 urlpatterns = [
-	path('exist/', views.does_account_exist_view, name="check_if_account_exists"),
-	path('register/', registration_view, name="register"),
-	path('login/', views.ObtainAuthTokenView.as_view(), name="login"),
-	path('logout/', views.User_logout, name="logout"),
-	path('DeleteAccount/', views.DeleteAccount.as_view(),name="DeleteAccount"),
-	path('verification/' , views.verification, name="verification"), 
-	path('change_password/', views.ForgetPasswordView.as_view(), name="change_password"),
+    path('exist/', views.does_account_exist_view, name="check_if_account_exists"),
+    path('register/', registration_view, name="register"),
+    path('login/', views.ObtainAuthTokenView.as_view(), name="login"),
+    path('logout/', views.User_logout, name="logout"),
+    path('DeleteAccount/', views.DeleteAccount.as_view(), name="DeleteAccount"),
+    path('verification/', views.verification, name="verification"),
+    path('change_password/', views.ForgetPasswordView.as_view(), name="change_password"),
+    path('verification/', views.verification, name="verification"),
+    path('edit-profile/', views.EditProfileView.as_view(), name='edit-profile')
 ]
-
-
 
 urlpatterns = format_suffix_patterns(urlpatterns)
