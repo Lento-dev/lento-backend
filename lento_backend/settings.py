3f2746e6520de202c516d80e08864df773d6a7e6
sina <sinaeskandari007@gmail.com>
2022-03-26 19:34:56 +0430

    Fix installed apps

--- lento_backend/settings.py
+++ lento_backend/settings.py
@@ -48,10 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-    'user_account',
-    'drf_yasg'
     'rest_framework' ,
     'corsheaders', 
     'user_account' ,
