1d4808345019e2902b8ec5c69e6517449298f5f4
vioghn <banafshehgholinejad802@gmail.com>
2022-04-22 18:05:58 +0430

    advertisement API

---
+++ advertisement/api/views.py
@@ -0,0 +1,116 @@
+from rest_framework import generics,status , viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import Advertisement , ServiceAd, FoodAd , animalAd , clothesAd 
+from advertisement.models import BaseAdvertisement , ServiceAdvertisement , FoodAdvertisement , AnimalAdvertisement ,ClothAdvertisement
+
+class Foodcreate(generics.CreateAPIView, viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FoodAd
+    queryset = FoodAdvertisement.objects.all()
+
+
+    def create(self, request, *args, **kwargs):
+        serializer_data = request.data.copy()
+        serializer_data.update({'owner':request.user.id})
+        serializer = self.get_serializer(data=serializer_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class Servicecreate(generics.CreateAPIView, viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ServiceAd
+    queryset = ServiceAdvertisement.objects.all()
+    def create(self, request, *args, **kwargs):
+        serializer_data = request.data.copy()
+        serializer_data.update({'owner':request.user.id})
+        serializer = self.get_serializer(data=serializer_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class animalcreate(generics.CreateAPIView, viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = animalAd
+    queryset = AnimalAdvertisement.objects.all()
+
+
+    def create(self, request, *args, **kwargs):
+        serializer_data = request.data.copy()
+        serializer_data.update({'owner':request.user.id})
+        serializer = self.get_serializer(data=serializer_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class clothcreate(generics.CreateAPIView, viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = clothesAd
+    queryset = ClothAdvertisement.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer_data = request.data.copy()
+        serializer_data.update({'owner':request.user.id})
+        serializer = self.get_serializer(data=serializer_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- advertisement/models.py
+++ advertisement/models.py
@@ -14,18 +14,14 @@ class BaseAdvertisement(models.Model):
     Title = models.CharField(max_length=100,blank=True , null = True)
     Description = models.CharField(max_length = 1000 , blank=True , null = True) 
     Image = models.ImageField(upload_location , blank=True, null = True)
-
-
-    class Meta:
-        ordering = ['created']
-
-
-
-
-class ClothAdvertisement(BaseAdvertisement): 
     province = models.CharField(max_length=100, blank=True , null = True) 
     City = models.CharField(max_length= 100 , blank= True , null=True)
     Address = models.CharField(max_length=500 , blank=True , null=True)
+    owner  = models.ForeignKey('user_account.Account', related_name='advertisement_owner', on_delete=models.CASCADE, blank=True )
+
+
+
+class ClothAdvertisement(BaseAdvertisement):   
     expiration_date = models.DateField(max_length=8, null=True, blank=True)
     cloth_status = models.CharField(max_length=30 , blank = True)
     for_men = models.BooleanField(default=False)
@@ -36,24 +32,32 @@ class ClothAdvertisement(BaseAdvertisement):
     ('under wear', 'under wear') , ('jackets/coats' , 'jackets/coats')  ]
     cloth_size =[('free size','free size') , ('Large') , ('Large') , ('medium' , 'medium'),('small', 'small'),
      ('extra large','extra large') ,('extra small','extra small') ]
+    
 
 
 class FoodAdvertisement(BaseAdvertisement):
     Food_type = models.CharField(max_length= 1000 , blank = True) 
-    expiration_date = models.DateField(max_length=8, null=True, blank=True)
+    expiration_date = models.DateField(null=True, blank=True)
+   
 
 
 class AnimalAdvertisement(BaseAdvertisement):
     animal_breed = models.CharField(max_length= 50 , blank= True)
     animal_age = models.CharField(max_length = 10 , blank = True)
     Health = models.CharField(max_length = 100 , blank = True)
-    handingover_reason = models.CharField(max_length= 100 , blank=True)
-
-
+    handingover_reason = models.CharField(max_length= 500 , blank=True)
+    
 
 
 class ServiceAdvertisement(BaseAdvertisement): 
-    service_type = models.CharField(max_length = 50)
+    service_type =[('dentistry','dentistry'),('medical','medical'),('piping' , 'piping') , ('masonry', 'masonry')
+    ,('gardening', 'gardening'), ('vetinery','vetinery'),('teaching', 'teaching'),('Psychology' , 'Psychology')]
+    expiration_date = models.DateField(max_length=8, null=True, blank=True)
+    
+   
+
+   
+
 
 
     
