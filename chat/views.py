3fe87f4806988e1a3fffc95039ffc08e73575db5
vioghn <banafshehgholinejad802@gmail.com>
2022-05-26 02:40:07 +0430

    chat models and consumer

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-#they all will change 
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+from .models import Chat, Contact
+User = get_user_model()
 
 
 def index(request):
@@ -12,3 +12,17 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+
+def get_last_10_messages(chatId):
+    chat = get_object_or_404(Chat, id=chatId)
+    return chat.messages.order_by('-timestamp').all()[:10]
+
+
+def get_user_contact(username):
+    user = get_object_or_404(User, username=username)
+    return get_object_or_404(Contact, user=user)
+
+
+def get_current_chat(chatId):
+    return get_object_or_404(Chat, id=chatId)
