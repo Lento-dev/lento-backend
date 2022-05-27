9fe42b2f6bc1077bbb831581e9ac15375007befb
sina <sinaeskandari007@gmail.com>
2022-05-27 20:13:24 +0430

    Add gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,13 @@
 echo -e "********** RUNNING MIGRATIONS **********"
 python3 manage.py migrate --noinput
 
-python3 manage.py runserver 0.0.0.0:8000
+echo -e "********** COLLECTING STATICFILES **********"
+python manage.py collectstatic --no-input
+
+if [[ "$DJANGO_DEPLOYMENT" == "1" ]]; then
+	echo -e "********** STARTING PRODUCTION SERVER **********"
+	gunicorn lento_backend.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-'
+elif [[ "$DJANGO_DEPLOYMENT" == "0" ]]; then
+	echo -e "********** STARTING DEVELOPMENT SERVER **********"
+	python3 manage.py runserver 0.0.0.0:8000
+fi
